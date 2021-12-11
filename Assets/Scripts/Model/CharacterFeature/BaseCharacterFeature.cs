73861d49bb66729c0f1ccd76271bb952ca3b3279
akotov777 <akotov777@gmail.com>
2021-12-12 00:41:29 +0500

    Added CharacterFeature abstraction

---
+++ Assets/Scripts/Model/CharacterFeature/BaseCharacterFeature.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+
+public abstract class BaseCharacterFeature
+{
+    #region Fields
+
+    internal bool _isActive = true;
+    internal Action OnEnable = () => { };
+    internal Action OnDisable = () => { };
+
+    #endregion
+
+
+    #region Properties
+
+    public bool IsActive
+    {
+        get { return _isActive; }
+        set
+        {
+            bool oldValue = _isActive;
+            _isActive = value;
+            if (oldValue != _isActive)
+            {
+                if (oldValue == true)
+                    OnDisable.Invoke();
+                else
+                    OnEnable.Invoke();
+            }
+        }
+    }
+
+    #endregion
+}
---
+++ Assets/Scripts/Model/CharacterFeature/FirstPersonMovement.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+
+
+public sealed class FirstPersonMovement : ExecutableCharacterFeature
+{
+    #region Fields
+
+    private Transform _character;
+
+    #endregion
+
+
+    #region ClassLifeCycles
+
+    public FirstPersonMovement(Transform charater)
+    {
+        _character = charater;
+    }
+
+    #endregion
+
+
+    #region IExecutable
+
+    public override void Execute()
+    {
+
+    }
+
+    #endregion
+}
