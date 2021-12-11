d47db851051a0dadd44b62882ff91e03941fc34d
akotov777 <akotov777@gmail.com>
2021-12-12 00:47:36 +0500

    Added CharacterMovementSettings in resources

---
+++ Assets/Scripts/CharacterMovementSettings.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+
+[CreateAssetMenu(fileName = "CharacterMovementSettings", menuName = "ScriptableObjects/CharacterMovementSettings")]
+class CharacterMovementSettings : ScriptableObject
+{
+    #region Fields
+
+    public float movementSpeed = 5.0f;
+
+    #endregion
+}
+
