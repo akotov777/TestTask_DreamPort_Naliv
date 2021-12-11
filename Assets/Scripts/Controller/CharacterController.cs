2b24b924eb80e9e431a15e2b8d313bd4d1000815
akotov777 <akotov777@gmail.com>
2021-12-12 00:21:45 +0500

    Added CharacterController

---
+++ Assets/Scripts/Controller/CharacterController.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+
+
+public sealed class CharacterController : IExecutable
+{
+    #region Fields
+
+    private Camera _camera;
+
+    #endregion
+
+
+    #region ClassLifeCycles
+
+    public CharacterController()
+    {
+        _camera = Camera.main;
+    }
+
+    #endregion
+
+
+    #region IExecutable
+
+    public void Execute()
+    {
+
+    }
+
+    #endregion
+}
