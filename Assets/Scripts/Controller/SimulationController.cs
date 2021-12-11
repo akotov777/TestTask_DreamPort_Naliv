925bd8f381a27ce72332be280289c8311bae7154
akotov777 <akotov777@gmail.com>
2021-12-11 18:46:24 +0500

    Added simulation controller

---
+++ Assets/Scripts/Controller/SimulationController.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+
+public sealed class SimulationController : MonoBehaviour
+{
+    #region Fields
+
+    private List<IExecutable> _executables = new List<IExecutable>();
+
+    #endregion
+
+
+    #region UnityMethods
+
+    private void Update()
+    {
+        Execute();
+    }
+
+    #endregion
+
+
+    #region Methods
+
+    public void Execute()
+    {
+        for (int i = 0; i < _executables.Count; i++)
+        {
+            _executables[i].Execute();
+        }
+    }
+
+    public void AddExecutable(IExecutable executable)
+    {
+        _executables.Add(executable);
+    }
+
+    public void RemoveExecutable(IExecutable executable)
+    {
+        if (_executables.Contains(executable))
+        {
+            _executables.Remove(executable);
+        }
+    }
+
+    #endregion
+}
