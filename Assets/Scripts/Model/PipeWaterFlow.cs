20c1f3b21070dcac91aa0ccd7bf23283fc277c79
akotov777 <akotov777@gmail.com>
2021-12-12 17:41:58 +0500

    Added particle system logic

---
+++ Assets/Scripts/Model/PipeWaterFlow.cs
@@ -0,0 +1,72 @@
+﻿using UnityEngine;
+using System;
+
+
+public sealed class PipeWaterFlow : MonoBehaviour, IRatioProvider
+{
+    #region Fields
+
+    [SerializeField, SerializeReference] private IRatioProvider _closingRatioProvider;
+    [SerializeField] private ParticleSystem _particles;
+    [SerializeField] private float _minRateOverTime;
+    [SerializeField] private float _maxRateOverTime;
+    [SerializeField] private float _minFlowSpeed;
+    [SerializeField] private float _maxFlowSpeed;
+    private Action _onRatioChanged;
+
+    #endregion
+
+
+    #region UnityMethods
+
+    private void Start()
+    {
+        _onRatioChanged = RatioChanged;
+    }
+
+    #endregion
+
+
+    #region Methods
+
+    private void RatioChanged()
+    {
+        _onRatioChanged.Invoke();
+        ChangeParticleParameters();
+    }
+
+    private void ChangeParticleParameters()
+    {
+        float newRateOverTime = Mathf.Lerp(
+                                           _maxRateOverTime,
+                                           _minRateOverTime,
+                                           _closingRatioProvider.GetRatio());
+
+        float newParticleSpeed = Mathf.Lerp(_maxFlowSpeed,
+                                            _minFlowSpeed,
+                                            _closingRatioProvider.GetRatio());
+
+        var emission = _particles.emission;
+        emission.rateOverTime = newRateOverTime;
+
+        var main = _particles.main;
+        main.startSpeed = newParticleSpeed;
+    }
+
+    #endregion
+
+
+    #region IRatioProvider
+
+    public Action OnRatioChanged
+    {
+        get { return _onRatioChanged; }
+    }
+
+    public float GetRatio()
+    {
+        return _closingRatioProvider.GetRatio();
+    }
+
+    #endregion
+}
--- Assets/Scripts/Model/Valve.cs
+++ Assets/Scripts/Model/Valve.cs
@@ -6,9 +6,9 @@ public sealed class Valve : MonoBehaviour, IExecutable, IRatioProvider
 {
     #region Fields
 
-    [SerializeField, Min(1)] private float maxAngle = 720;
-    [SerializeField] private GameObject objectToRotate;
-    [SerializeField] private bool isClosedOnStart;
+    [SerializeField, Min(1)] private float _maxAngle = 720;
+    [SerializeField] private GameObject _objectToRotate;
+    [SerializeField] private bool _isClosedOnStart;
     private Action _onRatioChanged;
     private Vector3 _previousPosition;
     private Vector3 _currentPosition;
@@ -26,7 +26,7 @@ public sealed class Valve : MonoBehaviour, IExecutable, IRatioProvider
     {
         get
         {
-            return _turnedAngle / maxAngle;
+            return _turnedAngle / _maxAngle;
         }
     }
 
@@ -56,10 +56,10 @@ public sealed class Valve : MonoBehaviour, IExecutable, IRatioProvider
 
     private void Start()
     {
-        if (isClosedOnStart)
+        if (_isClosedOnStart)
         {
-            objectToRotate.transform.Rotate(0, maxAngle, 0);
-            _turnedAngle = maxAngle;
+            _objectToRotate.transform.Rotate(0, _maxAngle, 0);
+            _turnedAngle = _maxAngle;
         }
 
         _onRatioChanged = () => { };
@@ -108,16 +108,16 @@ public sealed class Valve : MonoBehaviour, IExecutable, IRatioProvider
             _currentPosition = transform.InverseTransformPoint(_currentPosition);
 
             _angle = GetAngleInDegrees;
-            if (_turnedAngle + _angle > maxAngle)
+            if (_turnedAngle + _angle > _maxAngle)
             {
-                _angle = maxAngle - _turnedAngle;
+                _angle = _maxAngle - _turnedAngle;
             }
             else if (_turnedAngle + _angle < 0)
             {
                 _angle = -_turnedAngle;
             }
 
-            objectToRotate.transform.Rotate(0, _angle, 0);
+            _objectToRotate.transform.Rotate(0, _angle, 0);
             _turnedAngle += _angle;
             _previousPosition = _currentPosition;
             _onRatioChanged.Invoke();
